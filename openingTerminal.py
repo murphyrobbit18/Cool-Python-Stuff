975843244e64789d5ce3b3cbecd82dff5ea0b114
Robin Murphy <murphyrobbit18@gmail.com>
2024-11-18 12:22:18 -0330

    Delete openingTerminal.py

--- openingTerminal.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def process():
-    subprocess.call("start C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell", shell=True) 
-    # Here, I would like to write this in the new opened powershell : python ./hello_world.py (then press ENTER)
-
-process()
-input("end")
